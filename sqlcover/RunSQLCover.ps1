6f335548bf3bd91e61b322bf554121559f2daccb
nilesh.devdas@vinsys.com <nilesh.devdas@vinsys.com>
2020-06-03 02:49:11 +0530

    Coverage

--- sqlcover/RunSQLCover.ps1
+++ sqlcover/RunSQLCover.ps1
@@ -1,5 +1,5 @@
 . .\SQLCover.ps1
 $SQLCoverScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
 $SQLCoverDllFullPath =   $SQLCoverScriptDir  + "\SQLCover.dll"
-$result = Get-CoverTSql   $SQLCoverDllFullPath "server= 123.252.203.196,10007;User Id=sa; Password=sa@123 ;initial catalog=ScienceDatabase" "ScienceDatabase" "EXEC tSQLt.RunAll"
+$result = Get-CoverTSql   $SQLCoverDllFullPath "server= 123.252.203.196,10007;User Id=sa; Password=sa@123 ;initial catalog=SAMPLEUT" "SAMPLEUT "EXEC tSQLt.RunAll"
 Export-Html $result  $SQLCoverScriptDir
